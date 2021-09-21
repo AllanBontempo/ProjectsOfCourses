06dc1c7c392fbf967a279cb59011a0c41612a083
Allan Bontempo <allanbt@hotmail.com>
2021-09-20 23:09:17 -0300

    avançando em looping

--- linguagens/python/py-introducao-curso-1/advinhacao.py
+++ linguagens/python/py-introducao-curso-1/advinhacao.py
@@ -1,3 +1,27 @@
-print('Bem vindo ao jogo de Advinhação')
+print('********************************')
+print("Bem vindo ao jogo de Advinhação!")
+print('********************************\n')
 
+numero_secreto = 5
+total_de_tentativas = 3
 
+while total_de_tentativas > 0:
+
+    print("Você tem", total_de_tentativas, " tentativas ")
+
+    chute = int(input("Digite o seu número: "))
+
+    print("Você digitou:", chute)
+
+    if numero_secreto == chute:
+        print("Parabéns! O número está correto!\n")
+        break
+    else:
+        if chute > numero_secreto:
+            print("Você errou! Seu número é maior que o número secreto!\n")
+        elif chute < numero_secreto:
+            print("Você errou! O seu chute foi menor que o número secreto!\n")
+
+    total_de_tentativas = total_de_tentativas - 1
+
+print("Fim de jogo!")
