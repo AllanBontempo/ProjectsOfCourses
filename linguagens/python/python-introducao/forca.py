008ca85b8e2220e85da9cd72c34825a8c5879018
Allan Bontempo <allanbt@hotmail.com>
2021-10-09 17:32:42 -0300

    avançando na segunda parte do curso

--- linguagens/python/python-introducao/forca.py
+++ linguagens/python/python-introducao/forca.py
@@ -1,8 +1,37 @@
 def jogar():
 
-    print('********************************')
-    print("Bem vindo ao jogo de Forca!")
     print('********************************\n')
+    print("   Bem vindo ao jogo de Forca!  ")
+    print('********************************\n')
+
+
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("{}\n".format(letras_acertadas))
+        # print("  ____")
+        # print(" |    |")
+        # print(" |")
+        # print(" |")
+        # print(" |")
+        chute = input("Qual a letra? \n")
+        chute = chute.lower()
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute == letra.lower():
+               letras_acertadas[index] = letra
+            index = index + 1
+
+
 
 
     print("Fim de jogo!")
