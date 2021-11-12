f52e2d8b4557d46fc6a61c65bffe946015ffc3f1
Allan Bontempo <allanbt@hotmail.com>
2021-11-12 12:18:12 -0300

    Alurapic pt 1 - capitulo 3

--- linguagens/angular/alurapic/src/app/app.module.ts
+++ linguagens/angular/alurapic/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {CommonModule} from "@angular/common";
+
+import {PhotosModule} from "./photos/photos.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+    ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    PhotosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
