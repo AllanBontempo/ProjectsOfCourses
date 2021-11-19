645250ba3fe0443ad5ff5e15afc7f686ca5aa27a
Allan Bontempo <allanbt@hotmail.com>
2021-11-18 23:44:44 -0300

    Alurapic pt 1 - capitulo 6; Binding eventos; Pipe; RxJS

--- linguagens/angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ linguagens/angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import {Photo} from "../photo/photo";
-import {PhotoService} from "../photo/photo.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {debounceTime, Subject} from "rxjs";
+
+import {Photo} from "../photo/photo";
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-
-  title = 'alurapic';
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = []
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private photoService: PhotoService
-  ) {}
+    private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    // @ts-ignore
-    const userName = this.activatedRoute.snapshot.params.userName;
+    //@ts-ignore
+    this.photos = this.activatedRoute.snapshot.data.photos;
+  }
 
-    this.photoService.listFromUser(userName)
-      .subscribe(photo => this.photos = photo);
+  ngOnDestroy() {
+    this.debounce.unsubscribe();
+  }
+
+
+  filtrar(event: any) {
+
+    this.debounce.next(event.target.value);
+    this.debounce.pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 }
---
+++ linguagens/angular/alurapic/src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+
+import {PhotoService} from "../photo/photo.service";
+import {Photo} from "../photo/photo";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoListResolver implements Resolve<Observable<Photo[]>>{
+  constructor(private service: PhotoService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Photo[]> {
+
+    // @ts-ignore
+    const userName = route.params.userName;
+
+    return this.service.listFromUser(userName);
+  }
+}
