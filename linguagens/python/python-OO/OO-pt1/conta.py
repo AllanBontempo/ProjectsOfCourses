4e87344e3dd5999d375c16467c15c6184c35d4db
Allan Bontempo <allanbt@hotmail.com>
2022-01-12 15:29:11 -0300

    OO - pt 2 / aula 1 completa

--- linguagens/python/python-OO/OO-pt1/conta.py
+++ linguagens/python/python-OO/OO-pt1/conta.py
@@ -5,15 +5,14 @@ class Conta:
         self.titular =   titular
         self.saldo = saldo
         self.limite = limite
-        
 
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.saldo, self.titular))
 
     def depositar(self, valor):
         self.saldo += valor
-        print ("Seu novo saldo é de {}".format(self.saldo))
+        print("Seu novo saldo é de {}".format(self.saldo))
 
     def sacar(self, valor):
         self.saldo -= valor
-        print ("Seu novo saldo é de {}".format(self.saldo))
+        print("Seu novo saldo é de {}".format(self.saldo))
